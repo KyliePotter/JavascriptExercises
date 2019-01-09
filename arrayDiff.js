dcc4035792c2dc8bef2cee692c2ad5397cf5b2e3
kylie-potter <kylie.potter@kurtosys.com>
2019-01-09 23:52:17 +0200

    Array.diff Exercise

---
+++ arrayDiff.js
@@ -0,0 +1,12 @@
+// Your goal is to implement a difference function, which subtracts one list from another and returns the result.
+// It should remove all values from list a, which are present in list b.
+// array_diff([1,2],[1]) == [2] 
+// If a value is present in b, all of its occurrences must be removed from the other:
+// array_diff([1,2,2,2,3],[2]) == [1,3]
+
+function array_diff(array1, array2){
+    return array1 = array1.filter(values => values !== array2);
+}
+
+console.log(array_diff([1,2],1));
+console.log(array_diff([1,2,2,2,3],2));
